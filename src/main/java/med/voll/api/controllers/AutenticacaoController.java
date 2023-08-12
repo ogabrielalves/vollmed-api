33f46cdd70c2355c9288a6231dcd653449e20b6e
Gabriel Alves <gabrielalvessilva02@outlook.com>
2023-08-12 14:08:58 -0300

    [FEAT] - Criando a geração de token.

--- src/main/java/med/voll/api/controllers/AutenticacaoController.java
+++ src/main/java/med/voll/api/controllers/AutenticacaoController.java
@@ -1,8 +1,11 @@
 package med.voll.api.controllers;
 
 import jakarta.validation.Valid;
+import med.voll.api.dto.security.DadosTokenJWT;
 import med.voll.api.dto.usuario.DadosAutenticacao;
+import med.voll.api.models.Usuario;
 import med.voll.api.services.AutenticacaoService;
+import med.voll.api.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,10 +23,14 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         Authentication authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        String tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
