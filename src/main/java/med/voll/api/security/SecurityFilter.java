ab5c17d591f3f28482123acfed275a8569715924
Gabriel Alves <gabrielalvessilva02@outlook.com>
2023-08-12 17:25:45 -0300

    [FEAT] - Implementação do filtro de segurança para requisições na API.

---
+++ src/main/java/med/voll/api/security/SecurityFilter.java
@@ -0,0 +1,51 @@
+package med.voll.api.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.repositories.UsuarioRepository;
+import med.voll.api.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJwt = recuperarToken(request);
+        if (tokenJwt != null) {
+            String subject = tokenService.getSubject(tokenJwt);
+            UserDetails usuario = repository.findByLogin(subject);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    usuario,
+                    null,
+                    usuario.getAuthorities()
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String autorizhationHeader = request.getHeader("Authorization");
+        if (autorizhationHeader != null) {
+            return autorizhationHeader.replace("Bearer ", "").trim();
+        }
+        return null;
+    }
+}
