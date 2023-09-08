c257d0b4e8d333f5fe63e68b1e95353d79262965
Gabriel Alves <gabrielalvessilva02@outlook.com>
2023-09-08 05:17:04 -0300

    [FEAT] - Finalização da implementação da Consulta.

--- src/main/java/med/voll/api/dto/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/med/voll/api/dto/consulta/DadosDetalhamentoConsulta.java
@@ -1,5 +1,7 @@
 package med.voll.api.dto.consulta;
 
+import med.voll.api.models.Consulta;
+
 import java.time.LocalDateTime;
 
 public record DadosDetalhamentoConsulta(
@@ -8,4 +10,7 @@ public record DadosDetalhamentoConsulta(
         Long idPaciente,
         LocalDateTime dateTime
 ) {
+    public DadosDetalhamentoConsulta(Consulta consulta) {
+        this(consulta.getId(), consulta.getMedico().getId(),consulta.getPaciente().getId(), consulta.getData());
+    }
 }
